5a3622c7ffc74770aad88af2968b89e1aef70a13
Doniyorrr <doniyorqalandarov853@gmail.com>
2024-11-28 20:39:42 +0500

    added

---
+++ leedCode.js
@@ -0,0 +1,38 @@
+////twoSum
+// var twoSum = function(nums, target) {
+    //     let newArr = []
+    //     for(let i = 0; i < nums.length; i++){
+    //         for(let j = i+1; j < nums.length; j++){
+    //             if(nums[i] + nums[j] === target){
+    //                  newArr.push(i,j)
+    //             }
+    //         }
+    //     }
+    //     return newArr
+    // }
+    // console.log(twoSum([3,2,4],6));
+    
+    ////isPalindrome
+    // var isPalindrome = function(x) {
+    //     let newArr = []
+    //     let newArr2 = []
+    //    newArr = x.toString().split('')
+       
+    //     for (let j = newArr.length-1; j >= 0; j--) {
+    //         newArr2.push(newArr[j])
+    //         if (newArr[j] === newArr2[j]) {
+    //            return true
+    //     }
+    // }
+    // return false
+    // };
+    // console.log(isPalindrome(12121)); 
+
+    //// 
+    
+    // let ism = "doniyor"
+    // let ism2 = ism.slice(1)
+    // // console.log(ism2);
+    
+    // console.log(ism.charAt(0).toUpperCase() + ism2);
+    
